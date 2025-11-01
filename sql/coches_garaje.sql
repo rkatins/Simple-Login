2a364b27cf9229bf08b916c50188b10ea7be4f91
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2025-11-01 19:55:31 +0100

    Add files via upload

---
+++ sql/coches_garaje.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `coches` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `coches`;
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: coches
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `garaje`
+--
+
+DROP TABLE IF EXISTS `garaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `garaje` (
+  `id_coche` int(11) NOT NULL AUTO_INCREMENT,
+  `color` varchar(50) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_coche`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `garaje`
+--
+
+LOCK TABLES `garaje` WRITE;
+/*!40000 ALTER TABLE `garaje` DISABLE KEYS */;
+INSERT INTO `garaje` VALUES (1,'Rojo Race','Ford','Focus ST'),(2,'Azul Oscuro','Volkswagen','Golf R'),(3,'Gris Daytona','Audi','RS 3'),(4,'Negro Zafiro','BMW','M2 Competition'),(5,'Blanco Polar','Honda','Civic Type R'),(6,'Plata Iridio','Mercedes-AMG','C 43 Coupe'),(7,'Amarillo Sol','Toyota','GR86'),(8,'Rojo Fuego','Ford','Fiesta ST'),(9,'Azul Eléctrico','Volkswagen','Arteon R'),(10,'Verde Elixir','Hyundai','i20 N'),(11,'Gris Quantum','Audi','S4 Avant'),(12,'Naranja Furia','Cupra','León VZ'),(13,'Gris Sombra','Ford','Mustang GT'),(14,'Azul Noche','Volkswagen','Scirocco R'),(15,'Blanco Glaciar','Audi','TT S'),(16,'Negro Cosmos','BMW','M340i xDrive'),(17,'Rojo Clásico','Honda','Integra Type S'),(18,'Verde Jade','Porsche','Cayman S'),(19,'Azul Profundo','Ford','Puma ST'),(20,'Plata Ártico','Mercedes-AMG','A 35 4MATIC'),(21,'prueba','prueba','prueba');
+/*!40000 ALTER TABLE `garaje` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-01 19:54:00
